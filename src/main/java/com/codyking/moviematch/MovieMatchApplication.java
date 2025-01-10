2f9936a3a097f9da75d30cd35f42c3a7ed212131
Cody King <cody.king04@outlook.com>
2025-01-10 16:12:56 -0500

    Spring intiializer files added

---
+++ src/main/java/com/codyking/moviematch/MovieMatchApplication.java
@@ -0,0 +1,13 @@
+package com.codyking.moviematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMatchApplication.class, args);
+	}
+
+}
